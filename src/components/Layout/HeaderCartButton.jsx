87234a2066f4b182c1f7498a8eb4628d347fe8fe
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2021-09-02 16:55:01 +0530

    Added Bump effect using useEffect

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+
+  const [btnIsHighlighted, setIsButtonIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   },0)
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+  const { items } = cartCtx;
+  useEffect(() => {
+    if(cartCtx.items.length === 0){
+      return;
+    }
+    setIsButtonIsHighlighted(true);
+
+    const timer = setTimeout(()=>{
+      setIsButtonIsHighlighted(false);
+    },300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  }, [items])
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
